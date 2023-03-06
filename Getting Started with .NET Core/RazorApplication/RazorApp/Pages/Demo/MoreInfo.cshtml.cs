7aa091833f36d302fc076636d7e4e40ed4c00d49
Will Doy <William.Doy@dentonspensions.co.uk>
2023-03-06 16:10:14 +0000

    Razor Pages Web Application
    Getting Started with .NET Core \ Console Applications \ Razor Pages Web Application

---
+++ Getting Started with .NET Core/RazorApplication/RazorApp/Pages/Demo/MoreInfo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorApp.Pages.Demo
+{
+    public class MoreInfoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
