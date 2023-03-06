64790f128ee7255d2b2c90292805dd6a3a8fa395
Will Doy <William.Doy@dentonspensions.co.uk>
2023-03-06 15:05:00 +0000

    AppSettings File
    Getting Started with .NET Core \ Console Applications \ AppSettings File

--- Getting Started with .NET Core/OurFirstCoreConsole/ConsoleUI/Program.cs
+++ Getting Started with .NET Core/OurFirstCoreConsole/ConsoleUI/Program.cs
@@ -1,13 +1,30 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 
 namespace ConsoleUI
 {
-    internal class Program
+    class Program
     {
+        public static IConfiguration Configuration;
+
         static void Main(string[] args)
         {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            Configuration = builder.Build();
+
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello Getting Started Group!");
+
+            string connectionString = Configuration.GetConnectionString("Default");
+            Console.WriteLine($"Connection String: { connectionString }");
+
+            string otherInfo = Configuration.GetSection("OtherInfo").Value;
+            Console.WriteLine($"Other Info: { otherInfo }");
+
+            string demo1 = Configuration.GetSection("Demos").GetSection("Demo1").Value;
+            Console.WriteLine($"Demo 1: { demo1 }");
         }
     }
 }
