107dd87a7faccccd795c018986d2da31014dbaa6
Will Doy <William.Doy@dentonspensions.co.uk>
2023-03-06 15:22:37 +0000

    Working with Text Files Cross Platform
    Getting Started with .NET Core \ Console Applications \ Working with Text Files Cross Platform

--- Getting Started with .NET Core/OurFirstCoreConsole/ConsoleUI/Program.cs
+++ Getting Started with .NET Core/OurFirstCoreConsole/ConsoleUI/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace ConsoleUI
 {
@@ -9,22 +11,42 @@ namespace ConsoleUI
 
         static void Main(string[] args)
         {
-            var builder = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            //var builder = new ConfigurationBuilder()
+            //    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
 
-            Configuration = builder.Build();
+            //Configuration = builder.Build();
 
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello Getting Started Group!");
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello Getting Started Group!");
 
-            string connectionString = Configuration.GetConnectionString("Default");
-            Console.WriteLine($"Connection String: { connectionString }");
+            //string connectionString = Configuration.GetConnectionString("Default");
+            //Console.WriteLine($"Connection String: { connectionString }");
 
-            string otherInfo = Configuration.GetSection("OtherInfo").Value;
-            Console.WriteLine($"Other Info: { otherInfo }");
+            //string otherInfo = Configuration.GetSection("OtherInfo").Value;
+            //Console.WriteLine($"Other Info: { otherInfo }");
 
-            string demo1 = Configuration.GetSection("Demos").GetSection("Demo1").Value;
-            Console.WriteLine($"Demo 1: { demo1 }");
+            //string demo1 = Configuration.GetSection("Demos").GetSection("Demo1").Value;
+            //Console.WriteLine($"Demo 1: { demo1 }");
+
+            // .\Logs\LogFile.txt
+            string path = Path.Combine(".", "Logs");
+            string fullFile = Path.Combine(path, "LogFile.txt");
+
+            Directory.CreateDirectory(path);
+
+            List<string> logs = new List<string>
+            {
+                "This is my first log entry",
+                "This is my second log entry",
+                "Isn't he creative"
+            };
+
+            File.WriteAllLines(fullFile, logs);
+
+            foreach (string log in File.ReadAllLines(fullFile))
+            {
+                Console.WriteLine(log);
+            }
         }
     }
 }
