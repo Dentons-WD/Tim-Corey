393ae51428a0365f5301e58c8fe6eba6d05a8ff8
Will Doy <William.Doy@dentonspensions.co.uk>
2023-03-16 14:55:41 +0000

    MVC Web Application
    Getting Started with .NET Core \ Web Application Types \ MVC Web Application

---
+++ Getting Started with .NET Core/MVCApplication/MVCApp/Controllers/DemoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCApp.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MoreInfo()
+        {
+            return View();
+        }
+    }
+}
