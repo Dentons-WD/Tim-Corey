15822c4b022d3dad093daa44ee76c0f510f5dde9
Will Doy <William.Doy@dentonspensions.co.uk>
2023-03-16 15:21:14 +0000

    API Web Application
    Getting Started With .NET Core \  Web Application Types \ API Web Application

---
+++ Getting Started with .NET Core/APIApplication/APIApp/Controllers/MoreInfoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace APIApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoreInfoController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Info1", "Info2", "How Creative" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return $"Your ID is: { id }";
+        }
+    }
+}
