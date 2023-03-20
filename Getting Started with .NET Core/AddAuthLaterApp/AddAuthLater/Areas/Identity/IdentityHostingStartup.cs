840a759e507be7e536e000a964cef192f907a180
Will Doy <William.Doy@dentonspensions.co.uk>
2023-03-20 15:44:29 +0000

    Adding Authentication to an Existing Project
    Getting Started With .NET Core \ Authentication \ Adding Authentication to an Existing Project

---
+++ Getting Started with .NET Core/AddAuthLaterApp/AddAuthLater/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AddAuthLater.Areas.Identity.Data;
+using AddAuthLater.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AddAuthLater.Areas.Identity.IdentityHostingStartup))]
+namespace AddAuthLater.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AddAuthLaterContext>(options =>
+                    options.UseSqlite(
+                        context.Configuration.GetConnectionString("AddAuthLaterContextConnection")));
+
+                services.AddDefaultIdentity<AddAuthLaterUser>()
+                    .AddEntityFrameworkStores<AddAuthLaterContext>();
+            });
+        }
+    }
+}
