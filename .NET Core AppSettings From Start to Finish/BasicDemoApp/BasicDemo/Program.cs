15bd41372b0e066b6009b825cceb962f85bb58a7
Will Doy <William.Doy@dentonspensions.co.uk>
2023-03-21 16:54:58 +0000

    Azure Key Vault
    .NET Core AppSettings From Start to Finish \ Settings Security \ Azure Key Vault

--- .NET Core AppSettings From Start to Finish/BasicDemoApp/BasicDemo/Program.cs
+++ .NET Core AppSettings From Start to Finish/BasicDemoApp/BasicDemo/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -44,6 +47,21 @@ namespace BasicDemo
                     if (hostingContext.HostingEnvironment.IsDevelopment())
                     {
                         builder.AddUserSecrets<Program>();
+                    }
+
+                    if (hostingContext.HostingEnvironment.IsProduction())
+                    {
+                        var builtConfig = builder.Build();
+
+                        var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                        var keyVaultClient = new KeyVaultClient(
+                            new KeyVaultClient.AuthenticationCallback(
+                                azureServiceTokenProvider.KeyVaultTokenCallback));
+
+                        builder.AddAzureKeyVault(
+                            $"https://{builtConfig["KeyVaultName"]}.vault.azure.net/",
+                            keyVaultClient,
+                            new DefaultKeyVaultSecretManager());
                     }
 
                     builder.AddEnvironmentVariables();
