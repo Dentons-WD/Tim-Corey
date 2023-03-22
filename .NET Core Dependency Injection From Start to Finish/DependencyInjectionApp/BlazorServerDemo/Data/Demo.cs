9f626c5bfde45e1329346cf04ba940ecd6bab07a
Will Doy <William.Doy@dentonspensions.co.uk>
2023-03-22 15:43:11 +0000

    Creating a Demo Class
    .NET Core Dependency Injection From Start to Finish \ The Basics \ Creating a Demo Class

---
+++ .NET Core Dependency Injection From Start to Finish/DependencyInjectionApp/BlazorServerDemo/Data/Demo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BlazorServerDemo.Data
+{
+    public class Demo
+    {
+        public DateTime StartupTime { get; init; }
+
+        public Demo()
+        {
+            StartupTime = DateTime.Now;
+        }
+    }
+}
