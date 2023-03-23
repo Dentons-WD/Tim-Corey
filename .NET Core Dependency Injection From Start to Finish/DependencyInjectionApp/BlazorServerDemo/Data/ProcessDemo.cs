e610a963524111a7029ba35040aa7d420fb255ae
Will Doy <William.Doy@dentonspensions.co.uk>
2023-03-23 13:57:57 +0000

    Referencing a DI Object in Another Class
    .NET Core Dependency Injection From Start to Finish \ The Basics \ Referencing a DI Object in Another Class

---
+++ .NET Core Dependency Injection From Start to Finish/DependencyInjectionApp/BlazorServerDemo/Data/ProcessDemo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BlazorServerDemo.Data
+{
+    public class ProcessDemo
+    {
+        private readonly Demo _demo;
+
+        public ProcessDemo(Demo demo)
+        {
+            _demo = demo;
+        }
+
+        public int GetDaysInMonth()
+        {
+            return _demo.StartupTime.Month switch
+            {
+                1 => 31,
+                2 => 28,
+                3 => 31,
+                4 => 30,
+                5 => 31,
+                6 => 30,
+                7 => 31,
+                8 => 31,
+                9 => 30,
+                10 => 31,
+                11 => 30,
+                12 => 31,
+                _ => throw new IndexOutOfRangeException()
+            };
+        }
+    }
+}
--- .NET Core Dependency Injection From Start to Finish/DependencyInjectionApp/BlazorServerDemo/Startup.cs
+++ .NET Core Dependency Injection From Start to Finish/DependencyInjectionApp/BlazorServerDemo/Startup.cs
@@ -30,6 +30,7 @@ namespace BlazorServerDemo
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
             services.AddTransient<Demo>();
+            services.AddTransient<ProcessDemo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
