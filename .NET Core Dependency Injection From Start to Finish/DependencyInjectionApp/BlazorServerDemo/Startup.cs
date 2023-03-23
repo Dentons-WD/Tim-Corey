1b8cb1dcd3e8c63f320e7e745d2a0f1df4b576e1
Will Doy <William.Doy@dentonspensions.co.uk>
2023-03-23 13:32:37 +0000

    Adding a Class to Dependency Injection
    .NET Core Dependency Injection From Start to Finish \ The Basics \ Adding a Class to Dependency Injection

--- .NET Core Dependency Injection From Start to Finish/DependencyInjectionApp/BlazorServerDemo/Startup.cs
+++ .NET Core Dependency Injection From Start to Finish/DependencyInjectionApp/BlazorServerDemo/Startup.cs
@@ -29,6 +29,7 @@ namespace BlazorServerDemo
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddTransient<Demo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
