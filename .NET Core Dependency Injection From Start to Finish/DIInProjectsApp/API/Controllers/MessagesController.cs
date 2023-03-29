fefd538f6ee2f313cd2fd2d628d4050d9cd9828d
Will Doy <William.Doy@dentonspensions.co.uk>
2023-03-29 16:38:55 +0100

    ASP.NET Core
    .NET Core Dependency Injection From Start to Finish \ Dependency Injection In Common Project Types \ ASP.NET Core

---
+++ .NET Core Dependency Injection From Start to Finish/DIInProjectsApp/API/Controllers/MessagesController.cs
@@ -0,0 +1,27 @@
+﻿using DIDemoLib;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        private readonly IMessages _messages;
+
+        public MessagesController(IMessages messages)
+        {
+            _messages = messages;
+        }
+
+        // GET: api/<MessagesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { _messages.SayHello(), _messages.SayGoodBye() };
+        }
+    }
+}
