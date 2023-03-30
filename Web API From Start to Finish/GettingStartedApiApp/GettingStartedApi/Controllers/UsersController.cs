ca7dcb5d84f9df1dbd3ffb942fc09f86d9f68649
Will Doy <William.Doy@dentonspensions.co.uk>
2023-03-30 14:55:36 +0100

    HTTP Verbs
    Web API From Start to Finish \ Getting Started with API \ HTTP Verbs

--- Web API From Start to Finish/GettingStartedApiApp/GettingStartedApi/Controllers/UsersController.cs
+++ Web API From Start to Finish/GettingStartedApiApp/GettingStartedApi/Controllers/UsersController.cs
@@ -2,37 +2,53 @@
 
 namespace GettingStartedApi.Controllers;
 
+// CRUD - CREATE, READ, UPDATE, and DELETE
+// GET, POST, PUT, PATCH, DELETE
+
 [Route("api/[controller]")]
 [ApiController]
 public class UsersController : ControllerBase
 {
-    // GET: api/<UsersController>
+    // GET: api/Users
     [HttpGet]
     public IEnumerable<string> Get()
     {
         return new string[] { "value1", "value2" };
     }
 
-    // GET api/<UsersController>/5
+    // GET api/Users/5
     [HttpGet("{id}")]
     public string Get(int id)
     {
         return "value";
     }
 
-    // POST api/<UsersController>
+    // POST creates a new record
+    // POST api/Users
+    // https://localhost:7263/api/Users (POST)
     [HttpPost]
     public void Post([FromBody] string value)
     {
     }
 
-    // PUT api/<UsersController>/5
+    // PUT updates a whole record (or possibly creates)
+    // PUT api/Users/5
     [HttpPut("{id}")]
     public void Put(int id, [FromBody] string value)
     {
+        // FN, LN, Email, PhoneNumber
     }
 
-    // DELETE api/<UsersController>/5
+    // PATCH updates part of a record
+    // PATCH api/Users/5
+    [HttpPatch("{id}")]
+    public void Patch(int id, [FromBody] string emailAddress)
+    {
+
+    }
+
+    // DELETE deletes a record
+    // DELETE api/Users/5
     [HttpDelete("{id}")]
     public void Delete(int id)
     {
