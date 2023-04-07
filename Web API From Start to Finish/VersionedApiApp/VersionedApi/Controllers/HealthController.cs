80b9d62f6ae5090264f90a2d53a2a7dbbcadab03
Will Doy <ahzidol4@protonmail.com>
2023-04-07 15:23:52 +0100

    Version Neutral Endpoints
    Web API From Start to Finish \ Versioning an API \ Version Neutral Endpoints

---
+++ Web API From Start to Finish/VersionedApiApp/VersionedApi/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VersionedApi.Controllers;
+
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiController]
+[ApiVersionNeutral]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    [Route("ping")]
+    public IActionResult Ping()
+    {
+        return Ok("Everything seems great!");
+    }
+}
