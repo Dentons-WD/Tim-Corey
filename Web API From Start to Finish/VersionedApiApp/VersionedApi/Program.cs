f3897832a5402f7f3bdd53487248a50a57c0f190
Will Doy <ahzidol4@protonmail.com>
2023-04-07 15:00:21 +0100

    Configure Swagger
    Web API From Start to Finish \ Versioning an API \ Configure Swagger

--- Web API From Start to Finish/VersionedApiApp/VersionedApi/Program.cs
+++ Web API From Start to Finish/VersionedApiApp/VersionedApi/Program.cs
@@ -1,13 +1,58 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(opts =>
+{
+    var title = "Our Versioned API";
+    var description = "This is a Web API that demonstrates versioning.";
+    var terms = new Uri("https://localhost:7111/terms");
+    var license = new OpenApiLicense()
+    {
+        Name = "This is my full license information or a link to it."
+    };
+    var contact = new OpenApiContact()
+    {
+        Name = "Tim Corey Helpdesk",
+        Email = "help@iamtimcorey.com",
+        Url = new Uri("https://www.IAmTimCorey.com")
+    };
+
+    opts.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = $"{title} v1",
+        Description = description,
+        TermsOfService = terms,
+        License = license,
+        Contact = contact
+    });
+
+    opts.SwaggerDoc("v2", new OpenApiInfo
+    {
+        Version = "v2",
+        Title = $"{title} v2",
+        Description = description,
+        TermsOfService = terms,
+        License = license,
+        Contact = contact
+    });
+});
+
 builder.Services.AddApiVersioning(opts =>
 {
     opts.AssumeDefaultVersionWhenUnspecified = true;
     opts.DefaultApiVersion = new(1, 0);
     opts.ReportApiVersions = true;
+});
+
+builder.Services.AddVersionedApiExplorer(opts =>
+{
+    opts.GroupNameFormat = "'v'VVV";
+    opts.SubstituteApiVersionInUrl = true;
 });
 
 var app = builder.Build();
@@ -16,7 +61,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(opts =>
+    {
+        opts.SwaggerEndpoint("/swagger/v2/swagger.json", "v2");
+        opts.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    });
 }
 
 app.UseHttpsRedirection();
