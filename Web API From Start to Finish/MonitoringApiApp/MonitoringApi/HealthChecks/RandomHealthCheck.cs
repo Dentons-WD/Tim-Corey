a27fcbeb1056ae16b7e8053a0da75e0d9df87925
Will Doy <ahzidol4@protonmail.com>
2023-04-07 17:55:04 +0100

    Health Checks
    Web API From Start to Finish \ Monitoring an API \ Health Checks

---
+++ Web API From Start to Finish/MonitoringApiApp/MonitoringApi/HealthChecks/RandomHealthCheck.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace MonitoringApi.HealthChecks;
+
+public class RandomHealthCheck : IHealthCheck
+{
+    public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context,
+        CancellationToken cancellationToken = default)
+    {
+        int responseTimeInMs = Random.Shared.Next(300);
+
+        if (responseTimeInMs < 100)
+        {
+            return Task.FromResult(
+                HealthCheckResult.Healthy(
+                    $"The response time is excellent ({ responseTimeInMs }ms)"
+                    )
+                );
+        }
+        else if (responseTimeInMs < 200)
+        {
+            return Task.FromResult(
+                HealthCheckResult.Degraded(
+                    $"The response time is greater than expected ({ responseTimeInMs }ms)"
+                    )
+                );
+        }
+        else
+        {
+            return Task.FromResult(
+                HealthCheckResult.Unhealthy(
+                    $"The response time is unacceptable ({ responseTimeInMs }ms)"
+                    )
+                );
+        }
+    }
+}
--- Web API From Start to Finish/MonitoringApiApp/MonitoringApi/Program.cs
+++ Web API From Start to Finish/MonitoringApiApp/MonitoringApi/Program.cs
@@ -1,8 +1,24 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using MonitoringApi.HealthChecks;
+
+// https://github.com/Xabaril/AspNetCore.Diagnostics.HealthChecks
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks()
+    .AddCheck<RandomHealthCheck>("Site Health Check")
+    .AddCheck<RandomHealthCheck>("Database Health Check");
+
+builder.Services.AddHealthChecksUI(opts =>
+{
+    opts.AddHealthCheckEndpoint("api", "/health");
+    opts.SetEvaluationTimeInSeconds(5);
+    opts.SetMinimumSecondsBetweenFailureNotifications(10);
+}).AddInMemoryStorage();
 
 var app = builder.Build();
 
@@ -18,5 +34,10 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+app.MapHealthChecksUI();
 
 app.Run();
