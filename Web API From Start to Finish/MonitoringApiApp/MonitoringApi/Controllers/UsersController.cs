197a2b0272e6f418112dac9f1ed65fcb619df97e
Will Doy <ahzidol4@protonmail.com>
2023-04-07 16:56:19 +0100

    Logging
    Web API From Start to Finish \ Monitoring an API \ Logging

--- Web API From Start to Finish/MonitoringApiApp/MonitoringApi/Controllers/UsersController.cs
+++ Web API From Start to Finish/MonitoringApiApp/MonitoringApi/Controllers/UsersController.cs
@@ -1,41 +1,70 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace MonitoringApi.Controllers
+namespace MonitoringApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UsersController : ControllerBase
+    private readonly ILogger<UsersController> _logger;
+
+    public UsersController(ILogger<UsersController> logger)
     {
-        // GET: api/<UsersController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        _logger = logger;
+    }
 
-        // GET api/<UsersController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+    // GET: api/<UsersController>
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "value1", "value2" };
+    }
 
-        // POST api/<UsersController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
+    // GET api/<UsersController>/5
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        //if (id < 0 || id > 100)
+        //{
+        //    _logger.LogWarning("The given Id of {Identifier} was invalid.", id);
+        //    return BadRequest("The index was out of range.");
+        //}
 
-        // PUT api/<UsersController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        //_logger.LogInformation(@"The api\Users\{id} was called.", id);
+        //return Ok($"Value { id }");
 
-        // DELETE api/<UsersController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        try
         {
+            if (id < 0 || id > 100)
+            {
+                throw new ArgumentOutOfRangeException(nameof(id));
+            }
+
+            _logger.LogInformation(@"The api\Users\{id} was called.", id);
+            return Ok($"Value {id}"); 
         }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "The given Id of {Identifier} was invalid.", id);
+            return BadRequest("The index was out of range.");
+        }
+    }
+
+    // POST api/<UsersController>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/<UsersController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/<UsersController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
     }
 }
