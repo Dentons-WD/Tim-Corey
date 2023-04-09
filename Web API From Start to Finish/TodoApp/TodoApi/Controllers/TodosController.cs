35b5abd3e578644c5be1cb4d7901f01832e6ee86
Will Doy <ahzidol4@protonmail.com>
2023-04-09 19:13:30 +0100

    Create the Controllers
    Web API From Start to Finish \ Building an API \ Create the Controllers

---
+++ Web API From Start to Finish/TodoApp/TodoApi/Controllers/TodosController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoLibrary.Models;
+
+namespace TodoApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TodosController : ControllerBase
+{
+    // GET: api/Todos
+    [HttpGet]
+    public ActionResult<IEnumerable<TodoModel>> Get()
+    {
+        throw new NotImplementedException();
+    }
+
+    // GET api/Todos/5
+    [HttpGet("{id}")]
+    public ActionResult<TodoModel> Get(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    // POST api/Todos
+    [HttpPost]
+    public IActionResult Post([FromBody] string value)
+    {
+        throw new NotImplementedException();
+    }
+
+    // PUT api/Todos/5
+    [HttpPut("{id}")]
+    public IActionResult Put(int id, [FromBody] string value)
+    {
+        throw new NotImplementedException();
+    }
+
+    // PUT api/Todos/5/Complete
+    [HttpPut("{id}/Complete")]
+    public IActionResult Complete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    // DELETE api/Todos/5
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
