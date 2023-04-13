5f909d9d9b74435affac7e9e92db2616609b80a1
Will Doy <ahzidol4@protonmail.com>
2023-04-13 20:41:10 +0100

    Support OpenAPI 2
    Web API From Start to Finish \ Advanced OpenAPI Tasks \ Support OpenAPI 2

--- Web API From Start to Finish/SwaggerApiApp/SwaggerApi/Program.cs
+++ Web API From Start to Finish/SwaggerApiApp/SwaggerApi/Program.cs
@@ -7,7 +7,10 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
+    app.UseSwagger(opts =>
+    {
+        opts.SerializeAsV2 = true;
+    });
     app.UseSwaggerUI(opts =>
     {
         opts.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
