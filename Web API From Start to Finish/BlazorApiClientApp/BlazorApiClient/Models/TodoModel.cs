eb4c7f4f234c7f8d01a1fa72a68242760a4e0767
Will Doy <William.Doy@dentonspensions.co.uk>
2023-04-13 16:02:55 +0100

    Creating the Display Page
    Web API From Start to Finish \ Consuming an API \ Creating the Display Page

---
+++ Web API From Start to Finish/BlazorApiClientApp/BlazorApiClient/Models/TodoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApiClient.Models;
+
+public class TodoModel
+{
+    public int Id { get; set; }
+    public string? Task { get; set; }
+    public int AssignedTo { get; set; }
+    public bool IsComplete { get; set; }
+}
