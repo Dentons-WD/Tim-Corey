4ebcd6e6f1dbe60c24fba58373cd3bbc3b2f92a8
Will Doy <William.Doy@dentonspensions.co.uk>
2023-04-14 13:51:12 +0100

    Add Sample Endpoints
    Web API From Start to Finish \ Minimal APIs \ Add Sample Endpoints

--- Web API From Start to Finish/MinimalApiApp/MinimalApi/Program.cs
+++ Web API From Start to Finish/MinimalApiApp/MinimalApi/Program.cs
@@ -13,28 +13,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapGet("/api/Todo", () => new string[] { "string1", "string2" });
+app.MapGet("/api/Todo/{id}", (int id) => $"Id: { id }");
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
