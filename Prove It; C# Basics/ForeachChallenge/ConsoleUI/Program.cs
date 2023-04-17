35776912c40bf85c911a75ebe54811330b80f88b
Will Doy <William.Doy@dentonspensions.co.uk>
2023-04-17 14:14:43 +0100

    foreach Solution
    Prove It: C# Basics \ foreach \ foreach Solution

--- Prove It; C# Basics/ForeachChallenge/ConsoleUI/Program.cs
+++ Prove It; C# Basics/ForeachChallenge/ConsoleUI/Program.cs
@@ -10,6 +10,50 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            List<string> people = GetPeopleStrings();
+
+            foreach (string person in people)
+            {
+                Console.WriteLine($"Hello { person }");
+            }
+
+            Console.ReadLine();
+
+            List<PersonModel> peopleModels = GetPeopleModels();
+            
+            foreach (PersonModel person in peopleModels)
+            {
+                Console.WriteLine($"Hello { person.FirstName } { person.LastName }");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static List<PersonModel> GetPeopleModels()
+        {
+            List<PersonModel> output = new List<PersonModel>();
+
+            output.Add(new PersonModel { FirstName = "Tim", LastName = "Corey" });
+            output.Add(new PersonModel { FirstName = "Bill", LastName = "McCoy" });
+            output.Add(new PersonModel { FirstName = "Mary", LastName = "Jones" });
+            output.Add(new PersonModel { FirstName = "Sue", LastName = "Smith" });
+
+            return output;
+        }
+
+        private static List<string> GetPeopleStrings()
+        {
+            List<string> output = new List<string>();
+
+            output.Add("John");
+            output.Add("Mary");
+            output.Add("Sue");
+            output.Add("Greg");
+            output.Add("Yolada");
+            output.Add("Jose");
+            output.Add("Bill");
+
+            return output;
         }
     }
 }
