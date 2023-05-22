dbdbae63aaf786e78ec688096190fcdc9c620a6f
Will Doy <William.Doy@dentonspensions.co.uk>
2023-05-22 13:30:23 +0100

    How To Create Extension Methods in C#
    Practical C# \ How To Create Extension Methods in C#

--- Practical C#/ExtensionDemoApp/ExtensionDemo/Program.cs
+++ Practical C#/ExtensionDemoApp/ExtensionDemo/Program.cs
@@ -1,12 +1,54 @@
-﻿using System;
+﻿using CustomExtensions;
+using System;
 
 namespace ExtensionDemo
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            "This is a demo".PrintToConsole();
+
+            ISimpleLogger logger = new SimpleLogger();
+            logger.Log("Test Error", "ERror");
+            logger.LogError("This is an error");
+            logger.LogWarning("This is a warning.");
+            Console.WriteLine("Goodbye");
+
+            // person.Print().LogInfo().SaveToDatabase(myDBConnection);
+            // SaveToDatabase(LogInfo(Print(Person)));
+        }
+    }
+
+    public static class ExtendSimpleLogger
+    {
+        public static void LogError(this ISimpleLogger logger, string message)
+        {
+            var defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            logger.Log(message, "Error");
+            Console.ForegroundColor = defaultColor;
+        }
+
+        public static void LogWarning(this ISimpleLogger logger, string message)
+        {
+            var defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            logger.Log(message, "Warning");
+            Console.ForegroundColor = defaultColor;
+        }
+    }
+
+    public class SimpleLogger : ISimpleLogger
+    {
+        public void Log(string message)
+        {
+            System.Console.WriteLine(message);
+        }
+
+        public void Log(string message, string messageType)
+        {
+            Log($"{messageType}: {message}");
         }
     }
 }
