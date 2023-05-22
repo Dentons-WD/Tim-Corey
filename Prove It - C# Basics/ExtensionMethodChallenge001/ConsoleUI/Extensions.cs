551bba946638503fb13d4b10a923d65aaa562da6
Will Doy <William.Doy@dentonspensions.co.uk>
2023-05-22 14:00:49 +0100

    Extension Methods Challenge
    Prove It: C# Basics \ Extension Methods \ Extension Methods Challenge

---
+++ Prove It - C# Basics/ExtensionMethodChallenge001/ConsoleUI/Extensions.cs
@@ -0,0 +1,71 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    public static class Extensions
+    {
+        public static void Print(this string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static string Excite(this string message)
+        {
+            string output = message.Replace('.', '!');
+
+            return output;
+        }
+
+        public static PersonModel Fill(this PersonModel person)
+        {
+            PersonModel output = person;
+
+            Console.WriteLine("First Name:");
+            output.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Last Name:");
+            output.LastName = Console.ReadLine();
+
+            Console.WriteLine();
+
+            return output;
+        }
+
+        public static void Print (this PersonModel person)
+        {
+            Console.WriteLine($"First Name: { person.FirstName }");
+            Console.WriteLine($"Last Name: { person.LastName }");
+            Console.WriteLine();
+        }
+
+        public static double Add(this double number, double numberToAdd)
+        {
+            number = number + numberToAdd;
+
+            return number;
+        }
+
+        public static double Subract(this double number, double numberToSubtract)
+        {
+            number = number - numberToSubtract;
+
+            return number;
+        }
+
+        public static double MultiplyBy(this double number, double numberToMultiplyBy)
+        {
+            number = number * numberToMultiplyBy;
+
+            return number;
+        }
+
+        public static double DivideBy(this double number, double numberToDivideBy)
+        {
+            number = number / numberToDivideBy;
+
+            return number;
+        }
+    }
+}
--- Prove It - C# Basics/ExtensionMethodChallenge001/ConsoleUI/Program.cs
+++ Prove It - C# Basics/ExtensionMethodChallenge001/ConsoleUI/Program.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿ ﻿using System;
+using System.Security.Claims;
+
+// GOAL
+// Create two extension methods. One called Print that
+// prints a string to the console like so:
+// “HelloWorld”.Print() and one called Excite that
+// replaces all periods with exclamation points like so:
+// “Hello World.”.Excite()
+
+// BONUS
+// Create this chain for a Person object:
+// <person>.Fill().Print(); where Fill asks the user for each
+// property’s value and print will print all properties to
+// the console. Then create this chain on a double:
+// <double>.Add(4).Subtract(2).MultiplyBy(8).DivideBy(3);
 
 namespace ConsoleUI
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //"Hello World.".Excite().Print();
+
+            //PersonModel person = new PersonModel();
+            //person.Fill().Print();
+
+            double x = 3;
+            x = x.Add(5).Subract(2).MultiplyBy(8).DivideBy(3);
+
+            Console.WriteLine(x);
+
+            Console.ReadLine();
         }
     }
 }
