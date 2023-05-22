59a37859409a3bb64629c4d318febebc7685a103
Will Doy <William.Doy@dentonspensions.co.uk>
2023-05-22 14:34:42 +0100

    Extension Methods Solution
    Prove It: C# Basics \ Extension Methods \ Extension Methods Solution

--- Prove It - C# Basics/ExtensionMethodChallenge/ConsoleUI/Program.cs
+++ Prove It - C# Basics/ExtensionMethodChallenge/ConsoleUI/Program.cs
@@ -6,7 +6,19 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //string myName = "Timothy Corey.";
+            //"Hello World".Print();
+            //myName.Print();
+
+            //myName.Excite().Print();
+
+            //PersonModel person = new PersonModel();
+
+            //person.Fill().Print();
+
+            Console.WriteLine(4.15.Add(5).Subtract(2).MultiplyBy(8).DivideBy(3).Add(34));
+
+            Console.ReadLine();
         }
     }
 }
