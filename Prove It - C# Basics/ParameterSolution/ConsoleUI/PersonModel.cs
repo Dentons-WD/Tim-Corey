36d322bba596bc701c38ec7c9f3e1725b50b9dce
Will Doy <William.Doy@dentonspensions.co.uk>
2023-05-24 14:35:07 +0100

    Parameter Solution
    Prove It: C# Basics \ Parameter \ Parameter Solution

---
+++ Prove It - C# Basics/ParameterSolution/ConsoleUI/PersonModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleUI
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+    }
+}
--- Prove It - C# Basics/ParameterSolution/ConsoleUI/Program.cs
+++ Prove It - C# Basics/ParameterSolution/ConsoleUI/Program.cs
@@ -8,11 +8,67 @@ namespace ConsoleUI
 {
     class Program
     {
+        // Out variable
+        // Tuple
+        // 2 different ways to modify data in a method and not pass it back yet use it.
+
         static void Main(string[] args)
         {
+            //bool successfullyDivided = Divide(5, 3, out double result);
 
+            //Console.WriteLine($"The result is { result }");
+
+            //var mathResults = DoMath(5, 3);
+
+            //Console.WriteLine($"Add: { mathResults.additionResult }, Subtract: { mathResults.subtractionResult }");
+
+            //int age = 21;
+
+            //ProcessNumber(ref age);
+
+            //Console.WriteLine($"The new age is { age }");
+
+            PersonModel user = new PersonModel();
+
+            PopulateUser(user);
+
+            Console.WriteLine($"User: { user.FirstName } { user.LastName }");
 
             Console.ReadLine();
+        }
+
+        private static bool Divide(double x, double y, out double result)
+        {
+            if(y == 0)
+            {
+                result = 0;
+                return false;
+            }
+
+            result = x / y;
+
+            return true;
+        }
+
+        private static (double additionResult, double subtractionResult) DoMath(double x, double y)
+        {
+            (double additionResult, double subtractionResult) output = (0, 0);
+
+            output.additionResult = x + y;
+            output.subtractionResult = x - y;
+
+            return output;
+        }
+
+        private static void ProcessNumber(ref int value)
+        {
+            value *= 5;
+        }
+
+        private static void PopulateUser(PersonModel person)
+        {
+            person.FirstName = "Tim";
+            person.LastName = "Corey";
         }
     }
 }
