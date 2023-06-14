160c4cc4a804aac1309ab72474fe60b638136f4c
Will Doy <William.Doy@dentonspensions.co.uk>
2023-06-14 15:21:27 +0100

    Bulk File Renaming Solution
    Prove It: C# Basics \ Bulk File Renaming \ Bulk File Renaming Solution

--- Prove It - C# Basics/BulkRenamingChallengeApp/BulkRenamingConsole/Program.cs
+++ Prove It - C# Basics/BulkRenamingChallengeApp/BulkRenamingConsole/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
 
 namespace BulkRenamingConsole
 {
@@ -6,7 +9,53 @@ namespace BulkRenamingConsole
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Press enter to start...");
+            Console.ReadLine();
+
+            // Primary Challenge
+            var files = Directory.EnumerateFiles(@"C:\Demos\Bulk File Renaming\PrimaryChallengeFiles");
+
+            // file - C:\Demos\Bulk File Renaming\PrimaryChallengeFiles\acme by acme inc.txt
+            // fileName - acmeby acme inc
+            // filePath - C:\Demos\Bulk File Renaming\PrimaryChallengeFiles
+            // fileExtension - .txt
+
+            foreach(var file in files)
+            {
+                string fileName = Path.GetFileNameWithoutExtension(file);
+                string filePath = Path.GetDirectoryName(file);
+                string fileExtension = Path.GetExtension(file);
+
+                string newFileName = Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(fileName.ToLower());
+
+                newFileName = newFileName.Replace(" acme ", " TimCo ", StringComparison.InvariantCultureIgnoreCase);
+
+                if (newFileName.StartsWith("acme ", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    newFileName = "Timco " + newFileName.Substring(5);
+                }
+
+                if (newFileName.EndsWith(" acme", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    newFileName = newFileName.Substring(0, newFileName.Length - 5) + " TimCo";
+                }
+
+                File.Move(file, Path.Combine(filePath, newFileName + fileExtension));
+            }
+
+            // Bonus Challenge
+            var bonusFiles = Directory.EnumerateFiles(@"C:\Demos\Bulk File Renaming\BonusChallengeFiles");
+
+            foreach (var file in bonusFiles)
+            {
+                string filePath = Path.GetDirectoryName(file);
+                string fileExtension = Path.GetExtension(file);
+                string line1 = File.ReadLines(file).First();
+
+                string newFullNameAndPath = Path.Combine(filePath, line1 + fileExtension);
+
+                File.Move(file, newFullNameAndPath);
+            }
         }
     }
 }
