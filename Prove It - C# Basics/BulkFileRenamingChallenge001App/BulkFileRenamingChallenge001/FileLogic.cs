43a551fa779df105d4a22552f34a1749d822557c
Will Doy <William.Doy@dentonspensions.co.uk>
2023-06-14 14:35:47 +0100

    Bulk File Renaming Challenge
    Prove It: C# Basics \ Bulk File Renaming \ Bulk File Renaming Challenge

---
+++ Prove It - C# Basics/BulkFileRenamingChallenge001App/BulkFileRenamingChallenge001/FileLogic.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace BulkFileRenamingChallenge001
+{
+    public class FileLogic
+    {
+        public static string rootDir = @"C:\Demos\Bulk File Renaming\BonusChallengeFiles\";
+
+        public static void RenameFiles()
+        {
+            DirectoryInfo directory = new DirectoryInfo(rootDir);
+
+            List<FileInfo> files = directory.GetFiles().ToList();
+
+            //foreach (FileInfo file in files)
+            //{
+            //    string fileName = Path.GetFileNameWithoutExtension(file.FullName);
+            //    string extension  = Path.GetExtension(file.FullName);
+
+            //    fileName = TitleCaseString(fileName);
+
+            //    fileName = ReplaceAcmeWithTimCo(fileName);
+
+            //    fileName = $"{ rootDir }{ fileName }{ extension }";
+
+            //    file.MoveTo(fileName);
+            //}
+
+            List<string> lines = new List<string>();
+
+            foreach (FileInfo file in files)
+            {
+                lines = File.ReadAllLines(file.FullName).ToList();
+
+                string fileName = Path.GetFileNameWithoutExtension(file.FullName);
+                string extension = Path.GetExtension(file.FullName);
+
+                fileName = lines[0];
+
+                fileName = $"{ rootDir }{ fileName }{ extension }";
+
+                file.MoveTo(fileName);
+            }
+        }
+
+        public static string TitleCaseString(string input)
+        {
+            string output = "";
+
+            List<string> words = input.Split(' ').ToList();
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                string firstChar = words[i].Substring(0, 1).ToUpper();
+                string remainingChars = words[i].Substring(1).ToLower();
+
+                words[i] = $"{ firstChar }{ remainingChars }";
+            }
+
+            foreach (var word in words)
+            {
+                output = $"{ output } {word}";
+            }
+
+            output = output.Substring(1);
+
+            return output;
+        }
+
+        public static string ReplaceAcmeWithTimCo(string input)
+        {
+            string output = "";
+
+            output = input.Replace("Acme", "TimCo");
+
+            return output;
+        }
+    }
+}
--- Prove It - C# Basics/BulkFileRenamingChallenge001App/BulkFileRenamingChallenge001/Program.cs
+++ Prove It - C# Basics/BulkFileRenamingChallenge001App/BulkFileRenamingChallenge001/Program.cs
@@ -2,11 +2,23 @@
 
 namespace BulkFileRenamingChallenge001
 {
+    // GOAL
+    // Create a Console app that will rename every text file
+    // in a folder to be standard casing (first letter of each
+    // word capitalized). Also replace any instance of Acme
+    // with TimCo in the names.
+
+    // BONUS
+    // Using the files in the advanced folder, rename the
+    // files based upon the first line of each file. So, if File1.txt
+    // has a first line of Testing, the new file name should be
+    // Testing.txt. Make sure to retain the extension type.
+
     class Program
     {
         static void Main(string[] args)
         {
-            
+            FileLogic.RenameFiles();
         }
     }
 }
