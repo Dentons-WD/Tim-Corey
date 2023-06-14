77a3a54260902c1b0893ebe82e4aa836d233bedf
Will Doy <William.Doy@dentonspensions.co.uk>
2023-06-14 10:54:09 +0100

    Text File Challenge
    Prove It: C# Basics \ Text File \ Text File Challenge

--- Prove It - C# Basics/TextFileChallenge001App/TextFileChallenge001/ChallengeForm.cs
+++ Prove It - C# Basics/TextFileChallenge001App/TextFileChallenge001/ChallengeForm.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static TextFileChallenge.ReadWriteLogic;
 
 namespace TextFileChallenge
 {
@@ -18,6 +20,8 @@ namespace TextFileChallenge
         {
             InitializeComponent();
 
+            LoadUsers();
+
             WireUpDropDown();
         }
 
@@ -25,6 +29,30 @@ namespace TextFileChallenge
         {
             usersListBox.DataSource = users;
             usersListBox.DisplayMember = nameof(UserModel.DisplayText);
+        }
+
+        private void LoadUsers()
+        {
+            users = new BindingList<UserModel>(ReadUsers());
+        }
+
+        private void addUserButton_Click(object sender, EventArgs e)
+        {
+            UserModel newUser = new UserModel
+            {
+                FirstName = firstNameText.Text,
+                LastName = lastNameText.Text,
+                Age = (int)agePicker.Value,
+                IsAlive = isAliveCheckbox.Checked
+            };
+
+            users.Add(newUser);
+            WireUpDropDown();
+        }
+
+        private void saveListButton_Click(object sender, EventArgs e)
+        {
+            WriteUsers(users.ToList());
         }
     }
 }
---
+++ Prove It - C# Basics/TextFileChallenge001App/TextFileChallenge001/ReadWriteLogic.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileChallenge
+{
+    public static class ReadWriteLogic
+    {
+        //public static List<UserModel> ReadUsers()
+        //{
+        //    List<UserModel> users = new List<UserModel>();
+        //    List<string> lines = File.ReadAllLines(@"..\..\StandardDataSet.csv").ToList();
+
+        //    lines.RemoveAt(0);
+
+        //    foreach(string line in lines)
+        //    {
+        //        UserModel newUser = new UserModel();
+
+        //        var values = line.Split(',');
+
+        //        newUser.FirstName = values[0];
+        //        newUser.LastName = values[1];
+        //        newUser.Age = int.Parse(values[2]);
+
+        //        if (values[3] == "1")
+        //        {
+        //            newUser.IsAlive = true;
+        //        }
+        //        else
+        //        {
+        //            newUser.IsAlive = false;
+        //        }
+
+        //        users.Add(newUser);
+        //    }
+
+        //    return users;
+        //}
+
+        //public static void WriteUsers(List<UserModel> users)
+        //{
+        //    List<string> lines = new List<string>();
+
+        //    lines.Add("FirstName,LastName,Age,IsAlive");
+
+        //    foreach(var user in users)
+        //    {
+        //        string isAlive = "0";
+
+        //        if (user.IsAlive)
+        //        {
+        //            isAlive = "1";
+        //        }
+
+        //        lines.Add($"{ user.FirstName },{ user.LastName },{ user.Age },{ isAlive }");
+        //    }
+
+        //    File.WriteAllLines(@"..\..\StandardDataSet.csv", lines);
+        //}
+
+        public static (int firstNameIndex, int lastNameIndex, int ageIndex, int isAliveIndex) GetColumnIndexes()
+        {
+            List<string> lines = File.ReadAllLines(@"..\..\AdvancedDataSet.csv").ToList();
+
+            int FirstNameIndex = 0;
+            int LastNameIndex= 0;
+            int AgeIndex = 0;
+            int IsAliveIndex = 0;
+
+            List<string> columns = lines[0].Split(',').ToList();
+
+            for (int i = 0; i < columns.Count; i++)
+            {
+                switch (columns[i])
+                {
+                    case "FirstName":
+                        FirstNameIndex = i;
+                        break;
+                    case "LastName":
+                        LastNameIndex = i;
+                        break;
+                    case "Age":
+                        AgeIndex = i;
+                        break;
+                    case "IsAlive":
+                        IsAliveIndex = i;
+                        break;
+                }
+            }
+
+            return (FirstNameIndex, LastNameIndex, AgeIndex, IsAliveIndex);
+        }
+
+        public static List<UserModel> ReadUsers()
+        {
+            List<UserModel> users = new List<UserModel>();
+            List<string> lines = File.ReadAllLines(@"..\..\AdvancedDataSet.csv").ToList();
+
+            int FirstNameIndex = 0;
+            int LastNameIndex= 0;
+            int AgeIndex = 0;
+            int IsAliveIndex = 0;
+
+            (FirstNameIndex, LastNameIndex, AgeIndex, IsAliveIndex) = GetColumnIndexes();
+
+
+            lines.RemoveAt(0);
+
+            foreach(string line in lines)
+            {
+                UserModel newUser = new UserModel();
+
+                var values = line.Split(',');
+
+                newUser.FirstName = values[FirstNameIndex];
+                newUser.LastName = values[LastNameIndex];
+                newUser.Age = int.Parse(values[AgeIndex]);
+
+                if (values[IsAliveIndex] == "1")
+                {
+                    newUser.IsAlive = true;
+                }
+                else
+                {
+                    newUser.IsAlive = false;
+                }
+
+                users.Add(newUser);
+            }
+
+            return users;
+        }
+    }
+}
