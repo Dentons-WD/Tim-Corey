b72695b6e4238df38e62e3c0429d1d1e06ba815c
Will Doy <William.Doy@dentonspensions.co.uk>
2023-06-27 15:25:24 +0100

    WinFormUI- Initial Commit
    Extracted Solution from IntroToUnitTestingSourceCode.zip

---
+++ Practical C#/UnitTestsDemo/DemoLibrary/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public static class Calculator
+    {
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+
+        public static double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        public static double Divide(double x, double y)
+        {
+            return x / y; 
+        }
+    }
+}
---
+++ Practical C#/UnitTestsDemo/DemoLibrary/DataAccess.cs
@@ -0,0 +1,46 @@
+﻿using DemoLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public static class DataAccess
+    {
+        private static string personTextFile = "PersonText.txt";
+
+        public static void AddNewPerson(PersonModel person)
+        {
+            List<string> lines = new List<string>();
+            List<PersonModel> people = GetAllPeople();
+            people.Add(person);
+
+            foreach (PersonModel user in people)
+            {
+                lines.Add($"{ user.FirstName },{ user.LastName }");
+            }
+
+            File.WriteAllLines(personTextFile, lines);
+        }
+
+        public static List<PersonModel> GetAllPeople()
+        {
+            List<PersonModel> output = new List<PersonModel>();
+            string[] content = File.ReadAllLines(personTextFile);
+
+            foreach (string line in content)
+            {
+                string[] data = line.Split(',');
+                output.Add(new PersonModel { FirstName = data[0], LastName = data[1] });
+            }
+
+            return output;
+        }
+    }
+}
