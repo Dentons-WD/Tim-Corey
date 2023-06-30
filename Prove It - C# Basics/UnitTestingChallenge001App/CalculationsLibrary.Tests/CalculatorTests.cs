a6dee81e0051187f49cca47a0de4fcfeda0e2f47
Will Doy <William.Doy@dentonspensions.co.uk>
2023-06-30 10:04:16 +0100

    Unit Testing Challenge
    Prove It: C# Basics \ Unit Testing \ Unit Testing Challenge

---
+++ Prove It - C# Basics/UnitTestingChallenge001App/CalculationsLibrary.Tests/CalculatorTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CalculationsLibrary.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_SimpleAddition()
+        {
+            double expected = 18;
+
+            double actual = Calculator.Add(15,3);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Divide_SimpleDivision()
+        {
+            double expected = 5;
+
+            double actual = Calculator.Divide(15,3);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Divide_FailOnDivideByZero()
+        {
+            Assert.Throws<ArgumentException>(() => Calculator.Divide(15,0));
+        }
+
+        [Fact]
+        public void LimitedAdd_SimpleAddition()
+        {
+            double expected = 18;
+
+            double actual = Calculator.LimitedAdd(15,3, 0, 100);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void LimitedAdd_BelowMinimum()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Calculator.LimitedAdd(15,3, 10, 100));
+        }
+
+        [Fact]
+        public void LimitedAdd_AboveMaximum()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Calculator.LimitedAdd(15,3, 1, 10));
+        }
+    }
+}
--- Prove It - C# Basics/UnitTestingChallenge001App/ConsoleUI/Program.cs
+++ Prove It - C# Basics/UnitTestingChallenge001App/ConsoleUI/Program.cs
@@ -4,6 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// GOAL
+
+// Create tests for the Calculator class. Make sure to test
+// that the proper exceptions are thrown. Do not change
+// the code in the Calculator class.
+
+// BONUS
+//
+// Test the TextDataAccess class but do not permit the
+// system to write to an actual file. This means you will
+// need to rewrite the method to be more testable.
+// Make sure you test for the PathTooLongException.
+// Also, ensure that the filePath gets changed to just the
+// file name (no path).
+
 namespace ConsoleUI
 {
     internal class Program
