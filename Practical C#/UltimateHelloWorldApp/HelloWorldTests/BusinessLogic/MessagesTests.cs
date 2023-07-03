b0738e51c9d311a70fc20172f16a3fdb9b0ee403
Will Doy <William.Doy@dentonspensions.co.uk>
2023-07-03 12:24:21 +0100

    The Perfect C# Console Application...Or Not.
    Practical C# \ The Perfect C# Console Application...Or Not.

---
+++ Practical C#/UltimateHelloWorldApp/HelloWorldTests/BusinessLogic/MessagesTests.cs
@@ -0,0 +1,43 @@
+﻿using HelloWorldLibrary.BusinessLogic;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace HelloWorldTests.BusinessLogic;
+
+public class MessagesTests
+{
+    [Fact]
+    public void Greeting_InEnglish()
+    {
+        ILogger<Messages> logger = new NullLogger<Messages>();
+        Messages messages = new(logger);
+
+        string expected = "Hello World";
+        string actual = messages.Greeting("en");
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Greeting_InSpanish()
+    {
+        ILogger<Messages> logger = new NullLogger<Messages>();
+        Messages messages = new(logger);
+
+        string expected = "Hola Mundo";
+        string actual = messages.Greeting("es");
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Greeting_Invalid()
+    {
+        ILogger<Messages> logger = new NullLogger<Messages>();
+        Messages messages = new(logger);
+
+        Assert.Throws<InvalidOperationException>(
+            () => messages.Greeting("test")
+            );  
+    }
+}
---
+++ Practical C#/UltimateHelloWorldApp/UltimateHelloWorld/App.cs
@@ -0,0 +1,31 @@
+﻿using HelloWorldLibrary.BusinessLogic;
+
+namespace UltimateHelloWorld;
+
+public class App
+{
+    private readonly IMessages _messages;
+
+    public App(IMessages messages)
+    {
+        _messages = messages;
+    }
+
+    public void Run(string[] args)
+    {
+        string lang = "en";
+
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i].ToLower().StartsWith("lang="))
+            {
+                lang = args[i].Substring(5);
+                break;
+            }
+        }
+
+        string message = _messages.Greeting(lang);
+
+        Console.WriteLine(message);
+    }
+}
