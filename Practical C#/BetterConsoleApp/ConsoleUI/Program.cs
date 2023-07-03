3d8fd412692535178ec61257b254afae22d8f121
Will Doy <William.Doy@dentonspensions.co.uk>
2023-07-03 10:38:33 +0100

    .NET Core Console App with Dependency Injection, Logging, and Settings
    Practical C# \ .NET Core Console App with Dependency Injection, Logging, and Settings

--- Practical C#/BetterConsoleApp/ConsoleUI/Program.cs
+++ Practical C#/BetterConsoleApp/ConsoleUI/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.IO;
+
+// DI, Serilog, Settings
 
 namespace ConsoleUI
 {
@@ -6,7 +13,35 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Build())
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            Log.Logger.Information("Application Starting");
+
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddTransient<IGreetingService, GreetingService>();
+                })
+                .UseSerilog()
+                .Build();
+
+            var svc = ActivatorUtilities.CreateInstance<GreetingService>(host.Services);
+            svc.Run();
+        }
+
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{ Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production" }.json", optional: true)
+                .AddEnvironmentVariables();
         }
     }
 }
