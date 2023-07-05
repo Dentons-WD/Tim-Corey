f38af6fe6cf4aa387433479c8fde21ccb627e020
Will Doy <William.Doy@dentonspensions.co.uk>
2023-07-05 12:24:59 +0100

    ConsoleToDoList - Initial Commit
    Added ConsoleToDoList Console App to ConsoleToDoListChallenge Solution

---
+++ Prove It - C# Basics/ConsoleToDoListChallenge/ConsoleToDoList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleToDoList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
