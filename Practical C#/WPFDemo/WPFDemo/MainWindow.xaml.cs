1ac93812d865e343c555267167a1074893878a50
Will Doy <William.Doy@dentonspensions.co.uk>
2023-07-14 15:35:56 +0100

    WPF Controls: Learn basic WPF controls in C#
    Practical C# \ WPF Controls: Learn basic WPF controls in C#

--- Practical C#/WPFDemo/WPFDemo/MainWindow.xaml.cs
+++ Practical C#/WPFDemo/WPFDemo/MainWindow.xaml.cs
@@ -20,14 +20,36 @@ namespace WPFDemo
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Person> people = new List<Person>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            people.Add(new Person { FirstName = "Tim", LastName = "Corey" });
+            people.Add(new Person { FirstName = "Joe", LastName = "Smith" });
+            people.Add(new Person { FirstName = "Sue", LastName = "Storm" });
+
+            myComboBox.ItemsSource = people;
         }
 
         private void submitButton_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show($"Hello { firstNameText.Text }");
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return $"{ FirstName } { LastName }";
+            }
         }
     }
 }
