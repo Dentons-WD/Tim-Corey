6584e13d0823a2bb19df6a36b2ac30bc68d066c1
Will Doy <William.Doy@dentonspensions.co.uk>
2023-07-14 14:42:07 +0100

    Intro to WPF: Learn the basics and best practices of WPF for C#
    Practical C# \ Intro to WPF: Learn the basics and best practices of WPF for C#

--- Practical C#/WPFDemo/WPFDemo/MainWindow.xaml.cs
+++ Practical C#/WPFDemo/WPFDemo/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WPFDemo
         {
             InitializeComponent();
         }
+
+        private void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Hello { firstNameText.Text }");
+        }
     }
 }
