31cafeb582ab2a57cd9737b50d811144aab42837
Will Doy <ahzidol4@protonmail.com>
2023-07-15 16:31:53 +0100

    WPF in C# with MVVM using Caliburn Micro
    Practical C# \ WPF in C# with MVVM using Caliburn Micro

--- Practical C#/CMDemo/WPFUI/Views/SecondChildView.xaml.cs
+++ Practical C#/CMDemo/WPFUI/Views/SecondChildView.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WPFUI
+namespace WPFUI.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for SecondChildView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SecondChildView : UserControl
     {
-        public MainWindow()
+        public SecondChildView()
         {
             InitializeComponent();
         }
