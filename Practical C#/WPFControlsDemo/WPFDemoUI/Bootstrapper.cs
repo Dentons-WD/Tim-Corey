96175996e21c2e2521e24abf66cab76daee2e5c5
Will Doy <ahzidol4@protonmail.com>
2023-07-16 15:31:06 +0100

    WPFControlsDemo - Initial Commit
    Extracted Solution from WPFComboBox.zip

---
+++ Practical C#/WPFControlsDemo/WPFDemoUI/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WPFDemoUI.ViewModels;
+
+namespace WPFDemoUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ Practical C#/WPFControlsDemo/WPFDemoUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Caliburn.Micro;
+using DemoLibrary;
+using DemoLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFDemoUI.ViewModels
+{
+    public class ShellViewModel
+    {
+        public BindableCollection<PersonModel> People { get; set; }
+
+        public ShellViewModel()
+        {
+            DataAccess da = new DataAccess();
+            People = new BindableCollection<PersonModel>(da.GetPeople());
+        }
+    }
+}
