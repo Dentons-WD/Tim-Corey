6af8b1a2554db6b309f06ec03aaacfc12986e186
Will Doy <William.Doy@dentonspensions.co.uk>
2023-07-17 15:00:38 +0100

    WPF Controls with MVVM: ItemsControl
    Practical C# \ WPF Controls with MVVM: ItemsControl

--- Practical C#/WPFControlsDemo/DemoLibrary/DataAccess.cs
+++ Practical C#/WPFControlsDemo/DemoLibrary/DataAccess.cs
@@ -39,7 +39,7 @@ namespace DemoLibrary
             return output;
         }
 
-        private PersonModel GetPerson(int id)
+        public PersonModel GetPerson(int id)
         {
             PersonModel output = new PersonModel();
 
@@ -74,7 +74,7 @@ namespace DemoLibrary
             return output;
         }
 
-        private T GetRandomItem<T>(T[] data)
+        public T GetRandomItem<T>(T[] data)
         {
             return data[rnd.Next(0, data.Length)];
         }
--- Practical C#/WPFControlsDemo/WPFDemoUI/ViewModels/ShellViewModel.cs
+++ Practical C#/WPFControlsDemo/WPFDemoUI/ViewModels/ShellViewModel.cs
@@ -18,5 +18,33 @@ namespace WPFDemoUI.ViewModels
             DataAccess da = new DataAccess();
             People = new BindableCollection<PersonModel>(da.GetPeople());
         }
+
+        public void AddPerson()
+        {
+            DataAccess dataAccess = new DataAccess();
+
+            int maxId = 0;
+
+            if (People.Count > 0)
+            {
+                People.Max(x => x.PersonId);
+            }
+
+            People.Add(dataAccess.GetPerson(maxId + 1));                  
+        }
+
+        public void RemovePerson()
+        {
+            DataAccess dataAccess = new DataAccess();
+
+            if (People.Count == 0)
+            {
+                return;
+            }
+
+            PersonModel randomPerson = dataAccess.GetRandomItem(People.ToArray());
+
+            People.Remove(randomPerson);
+        }
     }
 }
