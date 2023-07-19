102f8db589f6e68713ffd8ba9a2e41f55db92122
Will Doy <William.Doy@dentonspensions.co.uk>
2023-07-19 12:36:39 +0100

    ToDo WPF Solution
    Prove It: C# Basics \ ToDo WPF \ ToDo WPF Solution

--- Prove It - C# Basics/WPFToDoChallengeApp/WPFToDo/MainWindow.xaml.cs
+++ Prove It - C# Basics/WPFToDoChallengeApp/WPFToDo/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+// https://github.com/punker76/gong-wpf-dragdrop
+
 namespace WPFToDo
 {
     /// <summary>
@@ -20,9 +23,82 @@ namespace WPFToDo
     /// </summary>
     public partial class MainWindow : Window
     {
+        BindingList<ToDoItemModel> todoList = new BindingList<ToDoItemModel>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            todoListBox.ItemsSource = todoList;
+        }
+
+        private void AddNewToDo_Click(object sender, RoutedEventArgs e)
+        {
+            ToDoItemModel todo = new ToDoItemModel
+            {
+                ToDoText = newToDoItem.Text
+            };
+
+            todoList.Add(todo);
+
+            newToDoItem.Text = "";
+        }
+
+        private void DeleteToDo_Click(object sender, RoutedEventArgs e)
+        {
+            if (todoListBox.SelectedItem == null)
+            {
+                return;
+            }
+
+            ToDoItemModel todo = (ToDoItemModel)todoListBox.SelectedItem;
+
+            todoList.Remove(todo);
+        }
+
+        ToDoItemModel editingItem = null;
+
+        private void EditToDo_Click(object sender, RoutedEventArgs e)
+        {
+            if (todoListBox.SelectedItem == null)
+            {
+                return;
+            }
+
+            editingItem = (ToDoItemModel)todoListBox.SelectedItem;
+
+            updateToDoItem.Text = editingItem.ToDoText;
+
+            editToDoPanel.Visibility = Visibility.Visible;
+            newToDoPanel.Visibility = Visibility.Collapsed;
+        }
+
+        private void UpdateToDo_Click(object sender, RoutedEventArgs e)
+        {
+            editingItem.ToDoText = updateToDoItem.Text;
+
+            editToDoPanel.Visibility = Visibility.Collapsed;
+            newToDoPanel.Visibility = Visibility.Visible;
+
+            updateToDoItem.Text = "";
+        }
+
+        private void TodoListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (todoListBox.SelectedItem == null)
+            {
+                return;
+            }
+
+            ToDoItemModel todo = (ToDoItemModel)todoListBox.SelectedItem;
+
+            todo.IsComplete = !todo.IsComplete;
+
+            if (todo.IsComplete)
+            {
+                todoList.Remove(todo);
+                todoList.Add(todo);
+            }
         }
     }
 }
---
+++ Prove It - C# Basics/WPFToDoChallengeApp/WPFToDo/ToDoItemModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace WPFToDo
+{
+    public class ToDoItemModel : INotifyPropertyChanged
+    {
+        private string _toDoText;
+        private bool _isComplete = false;
+
+        public string ToDoText
+        {
+            get => _toDoText;
+            set
+            {
+                _toDoText = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public bool IsComplete
+        {
+            get => _isComplete;
+            set
+            {
+                _isComplete = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
