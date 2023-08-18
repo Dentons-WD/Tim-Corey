d9011c2814ff89d80be8a9469f70354767cce6da
William Doy <william.doy@dentonspensions.co.uk>
2023-08-18 10:50:50 +0100

    Calling the Command Line in C# - Scripts, PowerShell, Automation, and more
    Practical C# \ Calling the Command Line in C# - Scripts, PowerShell, Automation, and more

--- Practical C#/CliDemoApp/CliDemo/Program.cs
+++ Practical C#/CliDemoApp/CliDemo/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CliWrap;
+using CliWrap.Buffered;
+
+//var dockerResults = await Cli.Wrap("docker")
+//    .WithArguments(new[] { "--version" })
+//    .ExecuteBufferedAsync();
+
+//Console.WriteLine(dockerResults.StandardOutput);
+
+//var gitResults = await Cli.Wrap("git")
+//    .WithArguments(new[] { "--version" })
+//    .ExecuteBufferedAsync();
+
+//Console.WriteLine(gitResults.StandardOutput);
+
+//var cmdResults = await Cli.Wrap(@"../../../../demo.bat")
+//    .ExecuteBufferedAsync();
+
+//Console.WriteLine(cmdResults.StandardOutput);
+
+var powershellResults = await Cli.Wrap(@"powershell")
+    .WithArguments(new [] { @"../../../../demo.ps1" })
+    .ExecuteBufferedAsync();
+
+Console.WriteLine(powershellResults.StandardOutput);
+
+Console.ReadLine();
